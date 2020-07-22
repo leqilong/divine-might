542b39bc0cd866c25d8311b2c146906e61ae9673
Leqi Long <leqilong@Leqis-MacBook-Pro.local>
2020-07-22 02:37:01 -0700

    Added assets

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import Phaser from "phaser";
+import skyImg from "./assets/sky.png";
+import platformImg from "./assets/platform.png";
+import starImg from "./assets/star.png";
+import bombImg from "./assets/bomb.png";
+import dudeSprite from "./assets/dude.png";
 
 const config = {
   type: Phaser.AUTO,
@@ -13,19 +18,23 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-// function preload() {
-//   this.load.image("logo", logoImg);
-// }
+function preload() {
+  this.load.image('sky', skyImg);
+  this.load.image('ground', platformImg);
+  this.load.image('star', starImg);
+  this.load.image('bomb', bombImg);
+  this.load.image('dude', dudeSprite, { frameWidth: 32, frameHeight: 48});
+}
 
-// function create() {
-//   const logo = this.add.image(400, 150, "logo");
-
-//   this.tweens.add({
-//     targets: logo,
-//     y: 450,
-//     duration: 2000,
-//     ease: "Power2",
-//     yoyo: true,
-//     loop: -1
-//   });
-// }
+function create() {
+  this.add.image(400, 300, 'sky');
+  this.add.image(400, 300, 'star');
+  // this.tweens.add({
+  //   targets: logo,
+  //   y: 450,
+  //   duration: 2000,
+  //   ease: "Power2",
+  //   yoyo: true,
+  //   loop: -1
+  // });
+}
